77af5e6cd669375143e9a9442caa0cbc5084d47a
Luke Anger <anger.luke2004@gmail.com>
2024-12-22 16:06:42 -0500

    removed dead code and added some comments in

--- navigation/deep-q-learning/src/tetris.py
+++ navigation/deep-q-learning/src/tetris.py
@@ -30,7 +30,6 @@ class Tetris:
     # These are the possible directions the bot can go
     directions = ((0, 1), (0, -1), (1, 0), (-1, 0))
 
-    unexplored_color = (0, 0, 0)
     explored_color = (255, 255, 255)
     robot_color = (255, 0, 0)
 
@@ -69,18 +68,11 @@ class Tetris:
         return sum((1 if p else 0) for row in grid for p in row)
 
     def reset(self):
-        # self.board = [[0] * self.width for _ in range(self.height)]
         self.score = 0
-        # self.tetrominoes = 0
-        # self.cleared_lines = 0
-        # self.bag = list(range(len(self.pieces)))
-        # random.shuffle(self.bag)
-        # self.ind = self.bag.pop()
-        # self.piece = [row[:] for row in self.pieces[self.ind]]
-        # self.current_pos = {"x": self.width // 2 - len(self.piece[0]) // 2, "y": 0}
         self.gameover = False
 
-        self.grid_heights = generate_ground(self.n, self.n, self.vertical_height)
+        # TODO: shouldn't change maps every time (its better to learn skills for specific map completely then move on), come up with a way to change maps every so often
+        # self.grid_heights = generate_ground(self.n, self.n, self.vertical_height)
         self.grid_explored_tracker = np.zeros((self.n, self.n), dtype=bool)
         self.robot_position = (0, 0)
 
@@ -102,14 +94,10 @@ class Tetris:
 
     # Use this function for a quick representation of the state
     def get_state_properties(self, possible_next_position):
-        # lines_cleared, board = self.check_cleared_rows(board)
-        # holes = self.get_holes(board)
-        # bumpiness, height = self.get_bumpiness_and_height(board)
 
         next_grid = self.get_updated_grid_explorer_tracker(self.grid_explored_tracker, possible_next_position)
         next_score: int = self.get_grid_explorer_tracker_score(next_grid)
 
-        # TODO: Add a time constraint to increase explorartion speed
         # TODO: Somewhere, maybe not here, add a constraint that the bot should always try to move
 
         return torch.FloatTensor([possible_next_position[0], possible_next_position[1], next_score])
@@ -127,57 +115,11 @@ class Tetris:
                 # Set the key to action and the value to a representation of the state
                 states[direction] = self.get_state_properties(possible_next_position)
 
-        # piece_id = self.ind
-        # curr_piece = [row[:] for row in self.piece]
-        # if piece_id == 0:  # O piece
-        #     num_rotations = 1
-        # elif piece_id == 2 or piece_id == 3 or piece_id == 4:
-        #     num_rotations = 2
-        # else:
-        #     num_rotations = 4
-
-        # for i in range(num_rotations):
-        #     valid_xs = self.width - len(curr_piece[0])
-        #     for x in range(valid_xs + 1):
-        #         piece = [row[:] for row in curr_piece]
-        #         pos = {"x": x, "y": 0}
-        #         while not self.check_collision(piece, pos):
-        #             pos["y"] += 1
-        #         self.truncate(piece, pos)
-        #         board = self.store(piece, pos)
-        #         states[(x, i)] = self.get_state_properties(board)
-        #     curr_piece = self.rotate(curr_piece)
         return states
 
     def step(self, action, render=True, video=None):
-        # x, num_rotations = action
-        # self.current_pos = {"x": x, "y": 0}
-        # for _ in range(num_rotations):
-        #     self.piece = self.rotate(self.piece)
-
-        # while not self.check_collision(self.piece, self.current_pos):
-        #     self.current_pos["y"] += 1
-        #     if render:
-        #         self.render(video)
-
         if render:
             self.render(video)
-
-        # overflow = self.truncate(self.piece, self.current_pos)
-        # if overflow:
-        #     self.gameover = True
-
-        # self.board = self.store(self.piece, self.current_pos)
-
-        # lines_cleared, self.board = self.check_cleared_rows(self.board)
-        # score = 1 + (lines_cleared ** 2) * self.width
-        # self.score += score
-        # self.tetrominoes += 1
-        # self.cleared_lines += lines_cleared
-        # if not self.gameover:
-        #     self.new_piece()
-        # if self.gameover:
-        #     self.score -= 2
 
         # update the time
         self.time_count += 1
