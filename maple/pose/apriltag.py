fe67f292fd1e8d400cf837b4f1bcf4812ab195d9
Robaire Galliath <galliath.robaire@gmail.com>
2024-12-31 15:01:12 -0800

    Implement coordinate transforms

--- maple/pose/apriltag.py
+++ maple/pose/apriltag.py
@@ -1,18 +1,9 @@
-# We can use apriltags on the lander to estimate our pose
-# We need to:
-#   - capture the geometry of the tags on the lander (from geometry.json)
-#   - combine pose estimations from multiple detections (average? maybe with accuracy threshold)
-#   - account for the potential offset of the lander from the global world coordinates
-
-# NOTE: We get the rovers initial pose from the simulator so we can use this to test if we are getting reasonable results
-# from looking at the apriltags on the lander (just in case something like the tagsize is wrong)
-# NOTE: Use pytransform3d for transformations
-
 import numpy as np
 from numpy.typing import NDArray
 from pyapriltags import Detector
 from pytransform3d.transformations import concat, invert_transform, transform_from
 from pytransform3d.rotations import matrix_from_euler
+from scipy.spatial.transform import Rotation
 
 from maple import geometry
 from maple.utils import camera_parameters, carla_to_pytransform
@@ -24,7 +15,7 @@ class Estimator:
     agent: None
     fiducials: dict
     detector: Detector
-    global_to_lander: NDArray
+    lander_global: NDArray
 
     def __init__(self, agent):
         """Create the estimator.
@@ -43,28 +34,43 @@ class Estimator:
         # At mission start we can get the position of the rover in the global coordinate frame
         # and the position of the lander in the rover's coordinate frame
         # using these we can determine the position of the lander in the global coordinate frame
-        global_to_rover = carla_to_pytransform(agent.get_initial_position())
-        rover_to_lander = carla_to_pytransform(agent.get_initial_lander_position())
-        self.global_to_lander = concat(global_to_rover, rover_to_lander)
+        rover_global = carla_to_pytransform(agent.get_initial_position())
+        lander_rover = carla_to_pytransform(agent.get_initial_lander_position())
+        self.lander_global = concat(lander_rover, rover_global)
+
+        # Correct for the apriltag coordinate convention
+        tag_correction = transform_from(
+            matrix_from_euler(
+                [np.pi / 2, 0, -np.pi / 2],
+                2,
+                1,
+                0,
+                False,
+            ),
+            [0, 0, 0],
+        )
 
         # We want a dict of tags (key: id, value: lander_to_tag)
         self.fiducials = {}
         for group, tag_group in geometry.lander["fiducials"].items():
             tag_rotations = {"a": -45, "b": 45, "c": 135, "d": -135}  # [degrees]
             rotation = matrix_from_euler(
-                [0, 0, np.deg2rad(tag_rotations[group])], 0, 1, 2, False
-            )  # TODO: This angle might need to be reversed
+                [np.deg2rad(tag_rotations[group]), 0, 0], 2, 1, 0, False
+            )
 
             # For each tag determine its transform from the lander
             for tag in tag_group.values():
                 translation = [tag["x"], tag["y"], tag["z"]]
-                lander_to_tag = transform_from(rotation, translation)
+                tag_lander = concat(
+                    tag_correction, transform_from(rotation, translation)
+                )
 
                 # Add this tag to the dict
-                self.fiducials[tag["id"]] = lander_to_tag
+                self.fiducials[tag["id"]] = concat(tag_lander, self.lander_global)
 
-        # The charging antenna locator is a special case we add
-        # TODO:
+    def __call__(self, input_data):
+        """Equivalent to calling `estimate`."""
+        return self.estimate(input_data)
 
     def estimate(self, input_data) -> NDArray:
         """Iterates through all active cameras and averages all detections.
@@ -89,29 +95,14 @@ class Estimator:
                 # Get the estimates from this image
                 estimates.extend(self._estimate_image(camera, image))
 
-        if not estimates:
+        if len(estimates) == 0:
             return None
 
-        # Average all the estimates
-        # Look at scipy rotation averaging
-        # For position we should probably just average each [x, y, z]
-        average = np.eye(4, 4)  # TODO: Implement
-        return average
-
-    def _global_to_tag(self, id) -> NDArray:
-        """Get the transform of a tag in the global frame.
-        Args:
-            id: the tag id
-
-        Returns:
-            The transform from the global frame to the tag frame
-        """
-
-        # TODO: lookup the tag position using geometry
-        lander_to_tag = np.eye(4, 4)
-
-        # TODO: Figure out the correct concat ordering
-        return concat(self.global_to_lander, lander_to_tag)
+        # Average the estimates
+        return transform_from(
+            Rotation.from_matrix([m[:3, :3] for m in estimates]).mean().as_matrix(),
+            np.mean([m[:3, 3] for m in estimates], axis=0),
+        )
 
     def _estimate_image(self, camera, image: NDArray[np.uint8]) -> list:
         """Process a single image to get a pose estimate.
@@ -125,8 +116,14 @@ class Estimator:
         """
 
         # camera is something like: carla.SensorPosition.Front
-        rover_to_camera = carla_to_pytransform(self.agent.get_camera_position(camera))
-        camera_to_rover = invert_transform(rover_to_camera)
+        camera_rover = carla_to_pytransform(self.agent.get_camera_position(camera))
+        rover_camera = invert_transform(camera_rover)
+
+        # Due to the coordinate axis convention AprilTag uses we need to apply a rotation correction
+        # Z is out of the camera, X is to the right of the image, Y is down in the image
+        camera_correction = transform_from(
+            matrix_from_euler([-np.pi / 2, 0, -np.pi / 2], 2, 1, 0, False), [0, 0, 0]
+        )
 
         detections = self.detector.detect(
             image, True, camera_parameters(image.shape), 0.339
@@ -135,19 +132,20 @@ class Estimator:
         estimates = []
         for detection in detections:
             # Calculate camera in tag coordinates
-            # TODO: Beware of the apriltag coordinate frame
-            # Z is out of the camera
-            # X is to the right of the image
-            # Y is down in the image
-            tag_to_camera = np.eye(4, 4)  # TODO: Implement
+            tag_camera = concat(
+                transform_from(detection.pose_R, detection.pose_t.ravel()),
+                camera_correction,
+            )
+            camera_tag = invert_transform(tag_camera)
 
             # Calculate the tag position in global coordinates
-            global_to_tag = self._global_to_tag(detection.tag_id)
+            try:
+                tag_global = self.fiducials[detection.tag_id]
+            except KeyError:
+                continue
 
-            # Concat transforms to get rover in world coordinates
-            global_to_rover = concat(
-                concat(global_to_tag, tag_to_camera), camera_to_rover
-            )
-            estimates.append(global_to_rover)
+            camera_global = concat(camera_tag, tag_global)
+            rover_global = concat(rover_camera, camera_global)
+            estimates.append(rover_global)
 
         return estimates
