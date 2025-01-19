8d8a7b10fb6eb6a1edc0e7a92f9b9ddd474f2c92
Robaire Galliath <galliath.robaire@gmail.com>
2025-01-19 10:40:11 -0500

    Create fixture for using GeometricMap

--- test/test_surface/test_surface_height.py
+++ test/test_surface/test_surface_height.py
@@ -3,6 +3,8 @@ import numpy as np
 from pytest import fixture
 from maple.surface.surface_height import sample_surface
 
+from dataclasses import dataclass
+
 
 @fixture
 def mock_geometric_map(mocker):
@@ -11,7 +13,18 @@ def mock_geometric_map(mocker):
     # Mock carla
     mocker.patch.dict(sys.modules, {"carla": mocker.MagicMock()})
 
-    # TODO: Mock GeometricMap
+    # Import after mocking carla
+    from leaderboard.agents.geometric_map import GeometricMap
+
+    @dataclass
+    class Constants:
+        map_size: float  # overall map width [m]
+        cell_size: float  # individual cell width [m]
+        cell_number: int  # number of cells [#]
+
+    geometric_map = GeometricMap(Constants(9, 0.15, 60))
+
+    return geometric_map
 
 
 def test_sample_surface():
@@ -27,3 +40,16 @@ def test_sample_surface():
     assert samples[3] == [-0.222, -0.203, -0.119]
 
     # TODO: Add tests with varied lander poses
+
+
+def test_map(mock_geometric_map):
+    """Demonstrates how to use the mock_geometric_map."""
+    gm = mock_geometric_map
+
+    assert gm.get_map_size() == 9
+    assert gm.get_cell_size() == 0.15
+    assert gm.get_cell_number() == 60
+
+    assert gm.get_cell_height(0, 0) == np.NINF
+    gm.set_cell_height(0, 0, 10)
+    assert gm.get_cell_height(0, 0) == 10
