d4670acd82363468a922ea4d581ca3cdc3fde9a4
Luke Anger <anger.luke2004@gmail.com>
2025-02-09 19:30:56 -0500

    navigation files added

---
+++ agents/dev.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+"""
+This module provides a human agent to control the ego vehicle via keyboard
+"""
+import time
+import json
+import math
+from numpy import random
+import numpy as np
+import cv2
+
+import carla
+
+from leaderboard.autoagents.autonomous_agent import AutonomousAgent
+
+from maple.pose.pose_estimator import Estimator
+
+from maple.navigation.navigator import Navigator
+
+def get_entry_point():
+    return 'Dev'
+
+
+class Dev(AutonomousAgent):
+
+    """
+    Spiral agent to start to develop from
+    """
+
+    def setup(self, path_to_conf_file):
+        """
+        Setup the agent parameters
+        """
+        self._active_side_cameras = False
+        self._active_side_front_cameras = True
+
+        self.estimator = Estimator(self)
+        self.navigatior = Navigator(self)
+
+    def use_fiducials(self):
+        return True
+    
+    def sensors(self):
+        """
+        Define which sensors are going to be active at the start.
+        The specifications and positions of the sensors are predefined and cannot be changed.
+        """
+        sensors = {
+            carla.SensorPosition.Front: {
+                'camera_active': True, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.FrontLeft: {
+                'camera_active': True, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.FrontRight: {
+                'camera_active': True, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.Left: {
+                'camera_active': True, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.Right: {
+                'camera_active': True, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.BackLeft: {
+                'camera_active': True, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.BackRight: {
+                'camera_active': True, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.Back: {
+                'camera_active': True, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+        }
+        return sensors
+
+    def run_step(self, input_data):
+        """Execute one step of navigation"""
+
+        control = carla.VehicleVelocityControl(0, 0.5)
+        front_data = input_data['Grayscale'][carla.SensorPosition.Front]  # Do something with this
+        if self._active_side_front_cameras:
+            front_left_data = input_data['Grayscale'][carla.SensorPosition.FrontLeft]  # Do something with this
+            front_right_data = input_data['Grayscale'][carla.SensorPosition.FrontLeft]  # Do something with this
+        if self._active_side_cameras:
+            left_data = input_data['Grayscale'][carla.SensorPosition.Left]  # Do something with this
+            right_data = input_data['Grayscale'][carla.SensorPosition.Right]  # Do something with this
+
+        mission_time = round(self.get_mission_time(), 2)
+
+        # Get a position estimate for the rover
+        estimate = self.estimator(input_data)
+
+        # IMPORTANT NOTE: The estimate should never be NONE!!!, this is test code to catch that
+        if estimate is None:
+            goal_lin_vel, goal_ang_vel = 10, 0
+            print(f'the estimate is returning NONE!!! that is a big problem buddy')
+        else:
+            # Get a goal linear and angular velocity from navigation
+            goal_lin_vel, goal_ang_vel = self.navigatior(estimate)
+
+            print(f'the estimate is {estimate}')
+            imu_data = self.get_imu_data()
+            print(f'the imu data is {imu_data}')
+
+        ##### This is test code
+        # from maple.utils import pytransform_to_tuple
+        # if estimate is not None:
+        #     _, _, _, _, _, yaw = pytransform_to_tuple(estimate)
+        #     print(f'the yaw is {yaw}')
+        ##### This is test code
+
+        # Set the goal velocities to be returned
+        control = carla.VehicleVelocityControl(goal_lin_vel, goal_ang_vel)
+        
+        return control
+
+    def finalize(self):
+        g_map = self.get_geometric_map()
+        map_length = g_map.get_cell_number()
+        for i in range(map_length):
+            for j in range(map_length):
+                g_map.set_cell_height(i, j, random.normal(0, 0.5))
+                g_map.set_cell_rock(i, j, bool(random.randint(2)))
---
+++ maple/navigation/path.py
@@ -0,0 +1,99 @@
+
+from math import hypot
+from shapely.geometry import LineString, Point
+
+# This is the path class which will be used to represent a path and have helper functions for navigation to be able to use it
+class Path:
+    """This class is designed to store the whole goal path which can be used later"""
+    # IMPORTANT NOTE: The straight line assumption is not permanent, need to build off of this later (I plan on doing that after RRT)
+
+    def __init__(self, target_locations):
+        """ This initializes a path
+
+        Args:
+            target_locations (tuple): Provide a list of locations like this [(0, 0), (1, 2), (3, 3), (6, 1)] and a path will be generated to go from one to the next
+        """
+
+        # Initialize the path with the points to go through
+        self.path = LineString(target_locations)
+
+        # Save the start location
+        self.start = target_locations[0]
+
+        # Save the end location
+        self.end = target_locations[-1]
+
+    def __call__(self):
+        return None
+
+    def get_start(self):
+        """
+        This function returns the start location
+        """
+
+        return self.start
+    
+    def get_end(self):
+        return self.end
+    
+    def get_distance_between_points(self, x1, y1, x2, y2):
+        return hypot(x1-x2, y1-y2)
+    
+    def get_next_point(self, current_point, step_size=.01):
+        """ This function uses the delta established in the init and a locaiton on the path to return the next point on the path
+
+        Args:
+            current_point (list): a list with our current x and y location
+            step_size (float, optional): _description_. Defaults to .01.
+
+        Returns:
+            list: The next (x, y) point to try and go towards
+        """
+
+        # Convert current_point to the point object to be used in the shapely code
+        current_point = Point(current_point)
+
+        # Find the nearest point on the path to the given current point
+        nearest_distance = self.path.project(current_point)  # Distance along the path
+        
+        # Calculate the next distance along the path
+        next_distance = nearest_distance + step_size
+
+        # Ensure we don't exceed the path length
+        if next_distance > self.path.length:
+            next_distance = self.path.length  # Stay at the endpoint
+
+        # Get the next point
+        next_point = self.path.interpolate(next_distance)
+        
+        # NOTE: This is to convert it out of the Point import, we can code this ourselves later for better efficiency
+        return (next_point.x, next_point.y)
+    
+    def traverse(self, start_loc_on_path, distance):
+        """
+        This function takes the location on a path and a goal distance to travel and will return the next point that is at least this distance away
+        """
+
+        # This counts the distance so we know how far we have gone
+        distance_count = 0
+
+        # This is the current position on the path we are
+        current_loc_on_path = start_loc_on_path
+
+        # Set up the while loop to repeat until we have traveled down the path long enough
+        while distance_count < distance:
+
+            # Get the next possible point on path
+            new_current_loc_on_path = self.get_next_point(current_loc_on_path)
+
+            # Add to the distance count so we can check if the goal distance has been met
+            distance_holder = self.get_distance_between_points(*current_loc_on_path, *new_current_loc_on_path)
+            distance_count += distance_holder
+
+            # NOTE: If distance holder doesnt change then we are too close to the end of the path to take another step so break the traverse
+            if distance_holder == 0:
+                break
+
+            current_loc_on_path = new_current_loc_on_path
+
+        return current_loc_on_path
