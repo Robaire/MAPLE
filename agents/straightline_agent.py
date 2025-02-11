c0de770e994a050564e22fca67dcfc2bb9eaa307
Cormac0 <50157706+Cormac0@users.noreply.github.com>
2025-02-10 22:25:30 -0500

    Created an agent that travels in a straight line and records pose estimates.

---
+++ agents/straightline_agent.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+"""
+This module provides a human agent to control the ego vehicle via keyboard
+"""
+import time
+import json
+import math
+from numpy import random
+
+import carla
+import csv
+
+from leaderboard.autoagents.autonomous_agent import AutonomousAgent
+
+from maple.pose.inertial_apriltag import InertialApriltagEstimator
+
+from maple.pose.inertial import InertialEstimator
+
+from maple.pose.apriltag import ApriltagEstimator
+from maple.utils import carla_to_pytransform
+
+
+def get_entry_point():
+    return 'DummyAgent'
+
+
+class DummyAgent(AutonomousAgent):
+
+    """
+    Dummy agent to showcase the different functionalities of the agent
+    """
+
+    def setup(self, path_to_conf_file):
+        """
+        Setup the agent parameters
+        """
+        self._active_side_cameras = False
+
+        self.InertialAprilTagEstimator = InertialApriltagEstimator(self)
+        self.InertialEstimator = InertialEstimator(self)
+        self.ApriltagEstimator = ApriltagEstimator(self)
+
+        self.actual_positions = []
+        self.estimated_positions = []
+        self.apriltag_positions = []
+        self.imu_positions = []
+        self.times = []
+
+    def use_fiducials(self):
+        return True
+
+    def sensors(self):
+        """
+        Define which sensors are going to be active at the start.
+        The specifications and positions of the sensors are predefined and cannot be changed.
+        """
+        sensors = {
+            carla.SensorPosition.Front: {
+                'camera_active': True, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.FrontLeft: {
+                'camera_active': False, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.FrontRight: {
+                'camera_active': False, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.Left: {
+                'camera_active': False, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.Right: {
+                'camera_active': False, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.BackLeft: {
+                'camera_active': False, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.BackRight: {
+                'camera_active': False, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.Back: {
+                'camera_active': False, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+        }
+        return sensors
+
+    def run_step(self, input_data):
+        """Execute one step of navigation"""
+
+        control = carla.VehicleVelocityControl(0, 0.5)
+        front_data = input_data['Grayscale'][carla.SensorPosition.Front]  # Do something with this
+        imu_data = self.get_imu_data()
+        if self._active_side_cameras:
+            left_data = input_data['Grayscale'][carla.SensorPosition.Left]  # Do something with this
+            right_data = input_data['Grayscale'][carla.SensorPosition.Right]  # Do something with this
+
+        mission_time = round(self.get_mission_time(), 2)
+
+        ia_estimate = self.InertialAprilTagEstimator(input_data)
+        i_estimate = self.InertialEstimator(input_data)
+        a_estimate = self.ApriltagEstimator(input_data)
+
+        if mission_time > 3 and mission_time <= 60:
+            self.estimated_positions.append(ia_estimate)
+            self.apriltag_positions.append(a_estimate)
+            self.imu_positions.append(i_estimate)
+            self.times.append(mission_time)
+            self.actual_positions.append(true_pose = carla_to_pytransform(self.get_transform())[:3, 3])
+            control = carla.VehicleVelocityControl(0.3, 0)
+
+        elif mission_time > 20:
+            self.mission_complete()
+
+        return control
+
+    def finalize(self):
+        g_map = self.get_geometric_map()
+        map_length = g_map.get_cell_number()
+        with open('data_output.csv', mode='w') as data_output:
+            data_writer = csv.writer(data_output)
+            data_writer.writerow(['Time', 'Actual X', 'Actual Y', 'Actual Z', 'Estimated X', 'Estimated Y', 'Estimated Z', 'Apriltag X', 'Apriltag Y', 'Apriltag Z', 'IMU X', 'IMU Y', 'IMU Z'])
+            for i in range(len(self.times)):
+                data_writer.writerow([self.times[i], self.actual_positions[i][0], self.actual_positions[i][1], self.actual_positions[i][2], self.estimated_positions[i][0], self.estimated_positions[i][1], self.estimated_positions[i][2], self.apriltag_positions[i][0], self.apriltag_positions[i][1], self.apriltag_positions[i][2], self.imu_positions[i][0], self.imu_positions[i][1], self.imu_positions[i][2])
+
+        for i in range(map_length):
+            for j in range(map_length):
+                g_map.set_cell_height(i, j, random.normal(0, 0.5))
+                g_map.set_cell_rock(i, j, bool(random.randint(2)))
