5cb943ae30392684091ca77bec7115d1a458e42a
Robaire Galliath <galliath.robaire@gmail.com>
2025-02-19 13:32:06 -0500

    Add MIT Agent

---
+++ agents/mit_agent.py
@@ -0,0 +1,171 @@
+import carla
+import numpy as np
+
+from pytransform3d.transformations import concat
+
+from maple.pose import InertialApriltagEstimator
+from maple.boulder import BoulderDetector
+from maple.navigation import Navigator
+from maple.surface.map import sample_surface
+
+from leaderboard.autoagents.autonomous_agent import AutonomousAgent
+
+
+def get_entry_point():
+    return "MITAgent"
+
+
+class MITAgent(AutonomousAgent):
+    """
+    The MIT Agent
+    """
+
+    def setup(self, path_to_conf_file):
+        """
+        Setup the agent parameters
+        """
+
+        # Frame counter
+        self.frame = 0
+
+        # Localization System
+        self.estimator = InertialApriltagEstimator(self)
+        # self.estimator = ApriltagEstimator(self)
+
+        # Boulder Detectors
+        self.front_detector = BoulderDetector(self, "FrontLeft", "FrontRight")
+        self.rear_detector = BoulderDetector(self, "BackLeft", "BackRight")
+
+        # Navigation
+        self.navigator = Navigator(self)
+
+        # Data Collection
+        self.boulders_global = []
+        self.surface_global = []
+
+    def use_fiducials(self):
+        return True
+
+    def sensors(self):
+        """
+        Enable the front and rear stereo cameras, side cameras, and associated lights.
+        Do not enable front and rear stand alone cameras or lights
+        """
+
+        sensors = {
+            carla.SensorPosition.Front: {
+                "camera_active": False,
+                "light_intensity": 0,
+                "width": "2448",
+                "height": "2048",
+            },
+            carla.SensorPosition.FrontLeft: {
+                "camera_active": True,
+                "light_intensity": 1,
+                "width": "2448",
+                "height": "2048",
+            },
+            carla.SensorPosition.FrontRight: {
+                "camera_active": True,
+                "light_intensity": 0,
+                "width": "2448",
+                "height": "2048",
+            },
+            carla.SensorPosition.Left: {
+                "camera_active": True,
+                "light_intensity": 1,
+                "width": "2448",
+                "height": "2048",
+            },
+            carla.SensorPosition.Right: {
+                "camera_active": True,
+                "light_intensity": 1,
+                "width": "2448",
+                "height": "2048",
+            },
+            carla.SensorPosition.BackLeft: {
+                "camera_active": True,
+                "light_intensity": 1,
+                "width": "2448",
+                "height": "2048",
+            },
+            carla.SensorPosition.BackRight: {
+                "camera_active": True,
+                "light_intensity": 0,
+                "width": "2448",
+                "height": "2048",
+            },
+            carla.SensorPosition.Back: {
+                "camera_active": False,
+                "light_intensity": 0,
+                "width": "2448",
+                "height": "2048",
+            },
+        }
+        return sensors
+
+    def run_step(self, input_data):
+        """Execute one step of navigation"""
+
+        # Pre-increment the frame
+        self.frame += 1
+
+        ## Mission Complete Check ##
+        # Check if there are conditions under which we would stop the simulation
+        if self.get_mission_time() > 60 * 5:  # Stop after 5 minutes
+            self.mission_complete()
+
+        ## Beginning Setup ##
+        if self.frame == 1:
+            # Move the arms out of the way
+            self.set_front_arm_angle(np.deg2rad(45))
+            self.set_back_arm_angle(np.deg2rad(45))
+
+        # Wait a few seconds for the arms to move into position
+        if self.get_mission_time() < 5:
+            return carla.VehicleVelocityControl(0.0, 0.0)
+
+        ## Localization ##
+        rover_global = self.estimator(input_data)
+        if rover_global is None:
+            # TODO: Figure out what to do when we don't have a pose estimate
+            # There is no reason to attempt data collection if we don't have a pose
+            # Perhaps we should utilize the last control input?
+            # Alternatively, Navigator may want to handle a None input and make a decision internally
+            return carla.VehicleVelocityControl(
+                self.linear_velocity, self.angular_velocity
+            )
+
+        ## Data Collection ##
+        if self.frame % 20 == 0:  # Sample at 1 Hz
+            # Get boulder detections
+            boulders_rover = []
+            boulders_rover.extend(self.front_detector(input_data))
+            boulders_rover.extend(self.rear_detector(input_data))
+
+            # Convert the boulders to the global frame
+            self.boulders_global.extend(
+                [concat(b_r, rover_global) for b_r in boulders_rover]
+            )
+
+        # Get surface height samples
+        self.surface_global.extend(sample_surface(rover_global))
+
+        # TODO: If the navigation is using interim boulder map or surface mapping data it can be processes here
+        # Although really this should be processed inside of the Navigator class
+        # Maybe it should take a reference to the global boulder list and surface map lists?
+        # self.navigator.update_boulders(self.boulders_global)
+
+        ## Navigation ##
+        self.linear_velocity, self.angular_velocity = self.navigator(rover_global)
+
+        return carla.VehicleVelocityControl(self.linear_velocity, self.angular_velocity)
+
+    def finalize(self):
+        # Calculate the final boulder map
+        # self.boulders_global
+
+        # Calculate the final surface height map (using boulders too!)
+        # self.surface_global
+
+        pass
