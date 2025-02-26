Fixed a radian/degree error in sample_surface, and added a quick function to test sample_surface in surface_testing.

maple/surface/map.py
@@ -111,8 +111,9 @@ def sample_surface(rover_global, pitch_roll_threshold = 90) -> list:
         A list of four ground sample points [x, y, z] (where each wheel touches the ground)
     """
     # Check if the rover is not tilted beyond the threshold
+    print("rover_global", rover_global)
     roll, pitch, yaw = pyrot.euler_from_matrix(rover_global[:3, :3],i=0,j=1,k=2,extrinsic=True)
-    if np.abs(pitch) > pitch_roll_threshold or np.abs(roll) > pitch_roll_threshold:
+    if np.abs(pitch) > np.deg2rad(pitch_roll_threshold) or np.abs(roll) > np.deg2rad(pitch_roll_threshold):
         return []
     
     samples = []