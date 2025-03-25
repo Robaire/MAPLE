df0c98c8dc999d7d615ba5d828ed7fe6cdacfbf4
Cormac0 <50157706+Cormac0@users.noreply.github.com>
2025-03-24 21:58:34 -0400

    First pass at a simple agent for testing the charging routine.
    Untested. Pushing so that it can be run on the simulator.

---
+++ agents/charging_agent.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+"""
+This module provides a human agent to control the ego vehicle via keyboard
+"""
+import time
+import json
+import math
+from numpy import random
+
+import carla
+
+from leaderboard.autoagents.autonomous_agent import AutonomousAgent
+
+from maple.boulder import BoulderDetector
+from maple.navigation import Navigator
+from maple.pose import InertialApriltagEstimator, PoseGraph
+from maple import utils
+from maple.utils import *
+from maple.surface.map import SurfaceHeight, sample_surface, sample_lander
+from maple.surface.post_processing import PostProcessor
+from maple.navigation.charging_navigator_straightshot import ChargingNavigator
+
+
+def get_entry_point():
+    return 'DummyAgent'
+
+
+class DummyAgent(AutonomousAgent):
+
+    """
+    Dummy agent to showcase the different functionalities of the agent
+    """
+
+    def setup(self, path_to_conf_file):
+        """
+        Setup the agent parameters
+        """
+        self._active_side_cameras = True
+        self.charging_flag = True
+        self.estimator = InertialApriltagEstimator(self)
+        self.charging_routine = ChargingNavigator(self)
+
+    def use_fiducials(self):
+        return True
+
+    def sensors(self):
+        """
+        Define which sensors are going to be active at the start.
+        The specifications and positions of the sensors are predefined and cannot be changed.
+        """
+        sensors = {
+            carla.SensorPosition.Front: {
+                'camera_active': True, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.FrontLeft: {
+                'camera_active': False, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.FrontRight: {
+                'camera_active': False, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.Left: {
+                'camera_active': True, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.Right: {
+                'camera_active': True, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.BackLeft: {
+                'camera_active': False, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.BackRight: {
+                'camera_active': False, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+            carla.SensorPosition.Back: {
+                'camera_active': False, 'light_intensity': 0, 'width': '2448', 'height': '2048'
+            },
+        }
+        return sensors
+
+    def run_step(self, input_data):
+        """Execute one step of navigation"""
+
+        estimate, is_april_tag_estimate = self.estimator(input_data)
+        imu_data = self.get_imu_data()
+
+        mission_time = round(self.get_mission_time(), 2)
+
+        if mission_time <= 3:
+            # Allow the vehicle to settle, and ensure lights are on
+            self.set_light_state(carla.SensorPosition.Front, 1.0)
+            self.set_light_state(carla.SensorPosition.Back, 1.0)
+            self.set_light_state(carla.SensorPosition.Left, 1.0)
+            self.set_light_state(carla.SensorPosition.Right, 1.0)
+            control = carla.VehicleVelocityControl(0, 0)
+
+        elif mission_time <= 120:
+            control, self.charging_flag = self.charging_routine.navigate(estimate)
+            control = carla.VehicleControl(control[0], control[1])
+
+        elif mission_time > 60:
+            self.mission_complete()
+
+        return control
+
+    def finalize(self):
+        g_map = self.get_geometric_map()
+        map_length = g_map.get_cell_number()
+        for i in range(map_length):
+            for j in range(map_length):
+                g_map.set_cell_height(i, j, random.normal(0, 0.5))
+                g_map.set_cell_rock(i, j, bool(random.randint(2)))
