5b14fcc611dd75a99da2d58f803c15655fba79cb
Robaire Galliath <galliath.robaire@gmail.com>
2025-04-27 20:16:02 -0400

    Add orbslam tests

--- test/test_pose/test_orbslam/test_orbslam.py
+++ test/test_pose/test_orbslam/test_orbslam.py
@@ -1,10 +1,86 @@
+from pytest import fixture, raises
+from lac_data import PlaybackAgent
+
 from maple.pose import OrbslamEstimator
-from test.mocks import mock_agent
+from maple.utils import pytransform_to_tuple
 
 
+@fixture
+def agent():
+    return PlaybackAgent("test/test_pose/test_orbslam/straight-line-1min.lac")
 
-def test_modes(mock_agent):
-    # for mode in ["mono", "stereo", "stereo_imu"]:
-    for mode in ["stereo_imu"]:
-        estimator = OrbslamEstimator(mock_agent, "FrontLeft", "FrontRight", mode=mode)
-        assert estimator is not None
+
+def _test_mode_error(agent):
+    with raises(ValueError):
+        estimator = OrbslamEstimator(agent, "FrontLeft", "FrontRight", mode="")
+
+
+def _test_mono_init(agent):
+    # Check if the camera is present
+    # with raises(ValueError):
+    #     estimator = OrbslamEstimator(agent, "Front", mode="mono")
+
+    # Check initialization
+    estimator = OrbslamEstimator(agent, "FrontLeft", mode="mono")
+    assert estimator is not None
+    estimator.shutdown()
+
+
+def _test_stereo_init(agent):
+    # Check if right camera is not provided
+    with raises(ValueError):
+        estimator = OrbslamEstimator(agent, "FrontLeft", mode="stereo")
+
+    # Check if the right camera is not present
+    # with raises(ValueError):
+    #     estimator = OrbslamEstimator(agent, "FrontLeft", "Front", mode="stereo")
+
+    # Check initialization
+    estimator = OrbslamEstimator(agent, "FrontLeft", "FrontRight", mode="stereo")
+    assert estimator is not None
+    estimator.shutdown()
+
+    # Check initialization
+    estimator = OrbslamEstimator(agent, "FrontLeft", "FrontRight", mode="stereo_imu")
+    assert estimator is not None
+    estimator.shutdown()
+
+
+def test_stereo(agent):
+    estimator = OrbslamEstimator(agent, "FrontLeft", "FrontRight", mode="stereo")
+    assert estimator is not None
+
+    frame = 90
+    agent.set_frame(frame)
+    while not agent.at_end():
+        input_data = agent.input_data()
+
+        estimate = estimator.estimate(input_data)
+        # assert estimate is not None
+        if estimate is not None:
+            print(f"Frame {frame}: {pytransform_to_tuple(estimate)}")
+        else:
+            print(f"Frame {frame}: None")
+            pass
+
+        frame = agent.step_frame()
+
+
+def _test_stereo_imu(agent):
+    # Check initialization
+    estimator = OrbslamEstimator(agent, "FrontLeft", "FrontRight", mode="stereo_imu")
+    assert estimator is not None
+
+    frame = 90
+    agent.set_frame(frame)
+    while not agent.at_end():
+        input_data = agent.input_data()
+
+        estimate = estimator.estimate(input_data)
+        # assert estimate is not None
+        if estimate is not None:
+            print(f"Frame {frame}: {pytransform_to_tuple(estimate)}")
+        else:
+            print(f"Frame {frame}: None")
+
+        frame = agent.step_frame()
