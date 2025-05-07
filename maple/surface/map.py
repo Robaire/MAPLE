afa851ac30cf285452a494f49845f983562d16ad
Robaire Galliath <galliath.robaire@gmail.com>
2025-05-07 01:13:08 -0400

    Remove print

--- maple/surface/map.py
+++ maple/surface/map.py
@@ -8,12 +8,6 @@ from maple.utils import carla_to_pytransform
 from maple import geometry
 
 
-# DO NOT IMPORT MATPLOTLIB IN CODE THE ROVER USES!!!!
-# import matplotlib
-# matplotlib.use("Agg")  # Set the backend before importing pyplot
-# import matplotlib.pyplot as plt
-
-
 class SurfaceHeight:
     def __init__(self, geometric_map):
         """
@@ -24,50 +18,11 @@ class SurfaceHeight:
         self.geometric_map = geometric_map
         self._last_height_map = None
 
-    # DO NOT USE MATPLOTLIB IN SurfaceHeight, PUT THIS IN A VISUALIZATION FILE
-    # def visualize_height_map(
-    #     self, height_map: NDArray = None, save_path: str = "height_map.png"
-    # ):
-    #     """Visualizes the height map using a color-coded plot and saves it to a file.
-
-    #     Args:
-    #         height_map: Optional pre-generated height map. If None, uses the last generated map.
-    #         save_path: Path where the plot should be saved. Defaults to 'height_map.png'
-    #     """
-    #     if height_map is None:
-    #         height_map = self._last_height_map
-
-    #     # Create a masked array to handle NINF values
-    #     masked_height_map = np.ma.masked_where(height_map == np.NINF, height_map)
-
-    #     plt.figure(figsize=(10, 8))
-    #     im = plt.imshow(masked_height_map.T, origin="lower", cmap="terrain")
-    #     plt.colorbar(im, label="Height")
-    #     plt.title("Surface Height Map")
-    #     plt.xlabel("X Cell Index")
-    #     plt.ylabel("Y Cell Index")
-    #     plt.savefig(save_path)  # Save the plot to file
-    #     plt.close()  # Close the figure to free memory
-
     def _generate_map(self, samples: list) -> NDArray:
         """Generates a 2D array of the average surface height for each cell."""
         size = self.geometric_map.get_cell_number()
         height_map = np.full((size, size), np.NINF)
         cell_counts = np.zeros((size, size))
-
-        # for sample in samples:
-        #     x, y, z = sample
-        #     cell_indexes = self.geometric_map.get_cell_indexes(x, y)
-        #     # print(cell_indexes)
-        #     # print('x:', x, 'y:', y, 'z:', z)
-
-        #     if cell_indexes is not None:
-        #         x_c, y_c = cell_indexes
-        #         if self.geometric_map._is_cell_valid(x_c, y_c):
-        #             if height_map[x_c, y_c] == np.NINF:
-        #                 height_map[x_c, y_c] = 0
-        #             height_map[x_c, y_c] += z
-        #             cell_counts[x_c, y_c] += 1
 
         nonzero_cells = cell_counts > 0
         height_map[nonzero_cells] /= cell_counts[nonzero_cells]
@@ -110,7 +65,7 @@ def sample_surface(rover_global, pitch_roll_threshold=60) -> list:
         A list of four ground sample points [x, y, z] (where each wheel touches the ground)
     """
     # Check if the rover is not tilted beyond the threshold
-    print("rover_global", rover_global)
+    # print("rover_global", rover_global)
     roll, pitch, yaw = pyrot.euler_from_matrix(
         rover_global[:3, :3], i=0, j=1, k=2, extrinsic=True
     )
@@ -132,31 +87,6 @@ def sample_surface(rover_global, pitch_roll_threshold=60) -> list:
         samples.append(surface_global[:3, 3].tolist())
 
     return samples
-
-
-# def sample_surface(rover_global) -> list:
-#     """Generates ground samples based on the pose of the rover.
-
-#     Args:
-#         rover_global: The pose of the rover in the global coordinate frame
-
-#     Returns:
-#         A list of four ground sample points [x, y, z] (where each wheel touches the ground)
-#     """
-
-#     samples = []
-#     for wheel in rover["wheels"].values():
-#         # The surface point in the rover frame
-#         surface_rover = pytrans.transform_from(
-#             np.eye(3), [wheel["x"], wheel["y"], wheel["z"] - (wheel["diameter"] / 2)]
-#         )
-
-#         surface_global = pytrans.concat(surface_rover, rover_global)
-
-#         # Append only the x, y, z components of the surface point
-#         samples.append(surface_global[:3, 3].tolist())
-
-#     return samples
 
 
 def sample_lander(agent):
